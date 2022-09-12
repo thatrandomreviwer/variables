342bae2e5be09111f51a9afbfed2594488d10aca
100133917 <100133917@lubbockisd.net>
2022-09-12 08:47:44 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+    public static void main(String[] args) {
+        //define 1 variable of each of the
+        //following data types
+        //byte	short		int 		long
+        //float	double
+        //char      boolean	String
+
+        //integer variables
+        byte byteOne = 127;
+        int x = 32;
+        short y = 12;
+        long q = 123414;
+        float f = 1246.32f;
+        double e = 321.435;
+        char d = 'v';
+        boolean p = true;
+        String A = "hello";
+
+        System.out.println("/////////////////////////////////");
+        System.out.println(byteOne);
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(q);
+        System.out.println(f);
+        System.out.println(e);
+        System.out.println(d);
+        System.out.println(p);
+        System.out.println(A);
+
+    }
+}
